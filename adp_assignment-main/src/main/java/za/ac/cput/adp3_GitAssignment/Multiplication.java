662729932d5d4ecaacc08f80aaf406c2ba7cae9c
Dinelle Kotze <219089302@mycput.ac.za>
2021-03-31 17:01:27 +0200

    initialisation

--- adp_assignment-main/src/main/java/za/ac/cput/adp3_GitAssignment/Multiplication.java
+++ adp_assignment-main/src/main/java/za/ac/cput/adp3_GitAssignment/Multiplication.java
@@ -7,5 +7,11 @@ package za.ac.cput.adp3_GitAssignment;
  * **/
 
 
-public class Multiplication {
+public class Multiplication
+{
+    public int multiplication(int x, int y)
+    {
+        return x * y;
+        //return 0 fails test
+    }
 }
